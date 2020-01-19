8163c43c99d6071bdc5e032463ac27e68cbe77b2
Michael Waldo <mbwcogic@yahoo.com>
2020-01-19 18:54:07 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,11 +4,11 @@ class EmailAddressParser
     @emails = emails
   end 
   def parse 
-    emails.split.collect do |address|
-      address.split(',') 
+    #emails.split.collect do |address|
+     # address.split(',') 
     end
-    .flatten.uniq 
-  end 
+    #.flatten.uniq 
+  #end 
 
 end
 
